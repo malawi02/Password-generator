853bb27cf74178caf015079436e25b425cbe9486
Zedekia Malawi <jatelo68@gmail.com>
2019-12-09 13:50:11 -0500

    update index.html

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// DOM elements
 const resultEL = document.getElementById('result');
 const lengthEL = document.getElementById('length');
 const uppercaseEL = document.getElementById('uppercase');
@@ -18,48 +17,83 @@ const randomFunc = {
 
 };
 
-generateEL.addEventListener('click', () => {
+generateEL.addEventListener('click',() => {
     const length = lengthEL.Value;
-    const hasLower = lowecaseEL.checked;
-    const hasUpper = lowecaseEL.checked;
-    const hasNumber = lowecaseEL.checked;
-    const hasSymbols = lowecaseEL.checked;
+    const hasLower = lowecaseEL;
+    const hasUpper = UppercaseEL;
+    const hasNumber = NumbersEL;
+    const hasSymbols = symbolsEL;
 
 
     resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbols,length);
-})
+});
 
-//Generate password function
+
+clipboardEL.addEventListener("click", ()  => {
+    const textarea = document.createElement("textarea");
+    const password = resultEl.innerText;
+    
+    if(!password){
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Password copied to clipboard");
+});
+
+
 function generatePassword(lower, upper, number, length){
-    //initialize pw var
-    //filter unchecked types
-    //loop over lenth call generator functions for each type
-    //Add final pw to the pw var and return
+    
+    
+    
+
 
 
     let generatePassword = '';
 
-    const typeCount = lower + upper + number + symbol;
+    const typesCount = lower + upper + number + symbol;
 
-    
+    const typesArr = [{lower}, {upper}, {number}, {symbols}].filter
+    (item => Object.values(item)[0]);
+
+    if(typesCount === 0) {
+        return '';
+    }
+
+    for(let i = 0; i < length; i += typesCount){
+    typesArr.forEach(type => {
+        const funcName = Object.keys(type)[0];
+
+        generatePassword += randomFunc[funcName]();
+    });
+}
+  
+const finalPassword = generatePassword.slice(0,length);
+
+
+return finalPassword;
 }
 
-// Generator functions - http://www.net-comber.com/charset.html
 
-//generate random password
+
+
 function getRandomLower(){
 
-    return String.fromCharCode(Math.floor(Math.random() * 8) + 128);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 
 
 }
 
 function getRandomUpper() {
-    return String.fromCharCoder(Math.floor(Math.random() * 8) + 128);
+    return String.fromCharCoder(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random()* 8)  +  128);
+    return String.fromCharCode(Math.floor(Math.random()* 10)  +  48);
 
 }
 
@@ -69,8 +103,6 @@ function getRandomSymbol() {
 }
 
      
-
-
 
 
 
